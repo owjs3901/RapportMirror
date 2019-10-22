778e90dde396dad859238c2a19b7306eb56d999e
M <owjs3901@naver.com>
2019-10-23 01:00:17 +0900

    create load activity and register activity

---
+++ app/src/main/java/kr/ac/innoschool/rapportmirror/LoadActivity.kt
@@ -0,0 +1,12 @@
+package kr.ac.innoschool.rapportmirror
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoadActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_load)
+    }
+}
