8f96cb9ee52420f0a37f6e644a5dc47d9c0543ba
M <owjs3901@naver.com>
2019-10-24 11:49:36 +0900

    layout

---
+++ app/src/main/java/kr/ac/innoschool/rapportmirror/ComplimentActivity.kt
@@ -0,0 +1,12 @@
+package kr.ac.innoschool.rapportmirror
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ComplimentActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_compliment)
+    }
+}
